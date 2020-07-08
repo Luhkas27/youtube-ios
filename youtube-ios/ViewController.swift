ac5efd3511dc56c1f2c77f062aef9697a5db068e
Lucas Cardoso <lucas.cardoso@opah.com.br>
2020-07-08 10:54:46 -0300

    initial xcode project creation

---
+++ youtube-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-ios
+//
+//  Created by Luhkas  on 08/07/20.
+//  Copyright © 2020 Luhkas . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
